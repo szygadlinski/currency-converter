55984b27003fcbf9aa648cb3a7d03f3a3334d7f5
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-12 23:15:56 +0200

    Add component CurrencyForm to Homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import styles from './Homepage.module.scss';
+
+import { CurrencyForm } from '../../features/CurrencyForm/CurrencyForm';
 
 const Component = ({ children }) => (
   <div className={styles.root}>
-    <h2>Homepage</h2>
-    {children}
+    <CurrencyForm />
   </div>
 );
 
