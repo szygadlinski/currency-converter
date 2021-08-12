5f975957ca2ae5ac7254a073833574a1bf0fcf6c
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-13 00:20:10 +0200

    Add content to CurrencyForm component

--- src/components/features/CurrencyForm/CurrencyForm.js
+++ src/components/features/CurrencyForm/CurrencyForm.js
@@ -1,16 +1,98 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+import { FormControl, OutlinedInput, InputLabel, Select, MenuItem, Button, Typography } from '@material-ui/core';
 import styles from './CurrencyForm.module.scss';
 
-const Component = ({ children }) => (
-  <div className={styles.root}>
-    <h2>CurrencyForm</h2>
-    {children}
-  </div>
-);
+const Component = () => {
 
-Component.propTypes = {
-  children: PropTypes.node,
+  const [conversion, setConversion] = useState({
+    amount: 0,
+    from: '',
+    to: '',
+  });
+
+  const changeConversion = event => {
+    setConversion({
+      ...conversion,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const convertCurrency = () => {
+
+  };
+
+  return (
+    <div className={styles.root}>
+      <form onSubmit={convertCurrency}>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='amount'>Amount</InputLabel>
+          <OutlinedInput
+            id='amount'
+            name='amount'
+            type='number'
+            label='Amount'
+            required
+            className={styles.input}
+            value={conversion.amount}
+            onChange={changeConversion}
+            inputProps={{
+              min: 0,
+              max: 999999999,
+            }}
+          />
+        </FormControl>
+
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='from'>From</InputLabel>
+          <Select
+            id='from'
+            name='from'
+            label='From'
+            required
+            className={styles.input}
+            value={conversion.from}
+            onChange={changeConversion}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='from'>To</InputLabel>
+          <Select
+            id='to'
+            name='to'
+            label='To'
+            required
+            className={styles.input}
+            value={conversion.to}
+            onChange={changeConversion}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          type='submit'
+          className={styles.input}
+          variant='contained'
+        >
+          <Typography variant="h6">
+            Convert
+          </Typography>
+        </Button>
+      </form>
+
+      <p>
+        100000 PLN = <strong>999999 EUR</strong>
+      </p>
+    </div>
+  );
 };
 
 export {
