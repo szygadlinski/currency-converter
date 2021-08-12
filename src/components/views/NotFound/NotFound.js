ba8a5abec5ffb24d0a1ebd1e9dd9f1a074327513
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-12 22:48:06 +0200

    Add content & styles to NotFound component

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Button } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styles from './NotFound.module.scss';
 
-const Component = ({ children }) => (
-  <div className={styles.root}>
-    <h2>NotFound</h2>
-    {children}
-  </div>
-);
+const Component = () => {
 
-Component.propTypes = {
-  children: PropTypes.node,
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDisplay(true);
+    }, 1000);
+  });
+
+  if(!display) {
+    return null;
+  } else return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>Error 404 - there is nothing here... :(</h2>
+
+      <Button
+        className={styles.return}
+        variant='contained'
+        component={NavLink}
+        to='/'
+        startIcon={<ArrowBackIcon className={styles.icon} />}
+      >
+        Back to the main page
+      </Button>
+    </div>
+  );
 };
 
 export {
